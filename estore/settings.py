90059df5cc3893a4e8722fe4a5466b67d8f22e23
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-07-28 16:31:27 +1000

    deleted shop app

--- estore/settings.py
+++ estore/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'shop',
     'store',
     'tags',
     'likes',
--- shop/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def greet_user(request):
-    return render(request, 'greet.html', { 'name': 'Matt'})
-
