2fd82236d158926e01e0351120262cd66d3c2050
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-07-28 21:12:38 +1000

    Migrated models to MySQL Database

--- estore/settings.py
+++ estore/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'estore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estore',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
--- estore/urls.py
+++ estore/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('shop/', include('shop.urls'))
 ]
