687fd50d24de39f86f909417c26159655ddd0613
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-09 21:35:24 +1000

    added filter to admin site

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 from . import models
 
 
+
+
 # Register your models here.
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name: str = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("<10", "Low")     
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     list_per_page: int = 20
     list_select_related = ["collection"]
+    list_filter = ["collection", "last_update", InventoryFilter]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -22,16 +43,45 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
     list_per_page: int = 20
     ordering = ["first_name", "last_name"]
- 
+    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+
+    @admin.display(ordering="orders_count")
+    def orders_count(self, customer):
+        url = (
+            reverse("admin:store_order_changelist")
+            + "?"
+            + urlencode({
+                "customer_id": str(customer.id),
+        }))
+        return format_html("<a href='{}'>{} Orders</a>", url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request) \
+            .annotate(orders_count= Count("order"))
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ["title"]
+    list_display = ["title", "products_count"]
     list_per_page: int = 20
+
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({
+                "collection__id": str(collection.id),
+            }))
+        return format_html("<a href='{}'>{}</a>", url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request) \
+            .annotate(products_count=Count("product"))
 
 
 @admin.register(models.Order)
