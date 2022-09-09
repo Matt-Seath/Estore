8586fe7c62aa3d613b3f863efbbc64b2e3244ea5
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-09 18:39:17 +1000

    register models in admin

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "unit_price"]
+    list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     list_per_page: int = 20
+    list_select_related = ["collection"]
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "OK"
+
+    def collection_title(self, product):
+        return product.collection.title
 
 
 @admin.register(models.Customer)
@@ -16,6 +27,16 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page: int = 20
     ordering = ["first_name", "last_name"]
  
-admin.site.register(models.Collection)
 
-admin.site.register(models.Order)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    list_per_page: int = 20
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "customer", "placed_at", "payment_status"]
+    list_editable = ["payment_status"]
+    list_per_page: int = 20
+    ordering = ["id", "customer", "placed_at"]
