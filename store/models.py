2e98b0c64824441d972851e4bd7915beb6b4dc4a
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-15 22:15:58 +1000

    created user profiles

--- store/models.py
+++ store/models.py
@@ -1,7 +1,9 @@
-from email import message
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.contrib import admin
+from django.db import models
+from django.conf import settings
 from uuid import uuid4
+from email import message
 
 # Create your models here.
 
@@ -52,19 +54,25 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     dob = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
+        return self.user.first_name + " " + self.user.last_name
+
+    @admin.display(ordering="user__first_name")
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering="user__last_name")
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ["first_name"]
+        ordering = ["user__first_name", "user__last_name"]
 
 
 class Order(models.Model):
