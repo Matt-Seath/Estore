4b56c287bf10502e5e0159bc3d8a651d61d3c853
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-17 15:32:40 +1000

    retrieve user profile from customer api endpoint

--- store/serializers.py
+++ store/serializers.py
@@ -6,7 +6,7 @@ from .models import *
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Customer
