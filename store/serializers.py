9e10a7729c80f61aeb2a878d2dbee0e81b0a1f43
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-18 21:04:35 +1000

    added file size validator

--- store/serializers.py
+++ store/serializers.py
@@ -23,10 +23,23 @@ class CollectionSerializer(serializers.ModelSerializer):
     products_count = serializers.IntegerField(read_only=True)
 
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        product_id = self.context["product_id"]
+        return ProductImage.objects.create(product_id=product_id, **validated_data)
+
+    class Meta:
+        model = ProductImage
+        fields = ["id", "image"]
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ["id", "title", "description", "slug", "inventory", "price", "price_with_tax", "collection"]
+        fields = ["id", "title", "description", "slug", "inventory",
+                    "price", "price_with_tax", "collection", "images"]
 
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
@@ -39,16 +52,6 @@ class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "title", "unit_price"]
-
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        product_id = self.context["product_id"]
-        return ProductImage.objects.create(product_id=product_id, **validated_data)
-
-    class Meta:
-        model = ProductImage
-        fields = ["id", "image"]
 
 
 class CartItemSerializer(serializers.ModelSerializer):
