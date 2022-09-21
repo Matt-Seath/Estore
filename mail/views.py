2c5f0da7af0dc700aab7b7d7100e50d71371b2d6
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-09-21 11:16:34 +1000

    crontab task scheduling

--- mail/views.py
+++ mail/views.py
@@ -1,22 +1,25 @@
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.http import HttpResponse
 from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
 # Create your views here.
 
 def email(request):
-    try:
-        message = BaseEmailMessage(
-            template_name="emails/hello.html",
-            context={"name": "Seath"}
-        )
-        file = "mail/static/images/4.jpg"
-        # message = EmailMessage("subject", "message", "from@gmail.com", ["to@gmail.com"])
-        # # send_mail("subject", "message", "john@gmail.com", ["doe@gmail.com"])
-        # # mail_admins("subject", "message", html_message="message")
-
-        # message.attach_file(file)
-        message.send(["to@gmail.com"])
-    except BadHeaderError:
-        pass
+    notify_customers.delay("Whats Up?")
     return HttpResponse("hi")
+
+# try:
+#     message = BaseEmailMessage(
+#         template_name="emails/hello.html",
+#         context={"name": "Seath"}
+#     )
+#     file = "mail/static/images/4.jpg"
+#     # message = EmailMessage("subject", "message", "from@gmail.com", ["to@gmail.com"])
+#     # # send_mail("subject", "message", "john@gmail.com", ["doe@gmail.com"])
+#     # # mail_admins("subject", "message", html_message="message")
+
+#     # message.attach_file(file)
+#     message.send(["to@gmail.com"])
+# except BadHeaderError:
+#     pass
